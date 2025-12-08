8b419d8ad10c79c0a415ed7dd82167693999c45c
m19e <m19e.dev@gmail.com>
2025-12-08 21:54:24 +0900

    💄 Demo tailwind / daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
+import { useState } from "react";
 
+// TODO fontconfigでtofu対応
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen min-w-screen gap-4 bg-gray-100 text-2xl gothic">
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+			<div className="bg-base-200 flex items-center justify-center">
+				<button type="button" className="btn btn-primary btn-lg mincho">
+					縦書きエディタテンプレ
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
