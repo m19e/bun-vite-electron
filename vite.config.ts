e97659898345295c581fbef95e61e32707f8da58
m19e <m19e.dev@gmail.com>
2025-12-08 14:50:06 +0900

    🔧 Activate tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
+// TODO activate Tailwind CSS
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
