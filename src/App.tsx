67c0e51d34e643c819db9e83c685464eabca7290
m19e <m19e.dev@gmail.com>
2025-12-09 15:45:31 +0900

    🎨 Sort classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 // TODO fontconfigでtofu対応
+// TODO biomeでclassNameソートできるか試す
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen min-w-screen gap-4 bg-gray-100 text-2xl gothic">
+		<div className="gothic flex min-h-screen min-w-screen flex-col items-center justify-center gap-4 bg-gray-100 text-2xl">
 			<h1>Vite + React</h1>
 			<div className="card">
 				<button type="button" onClick={() => setCount((count) => count + 1)}>
@@ -18,7 +19,7 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
-			<div className="bg-base-200 flex items-center justify-center">
+			<div className="flex items-center justify-center bg-base-200">
 				<button type="button" className="btn btn-primary btn-lg mincho">
 					縦書きエディタテンプレ
 				</button>
